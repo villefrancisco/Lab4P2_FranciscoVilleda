17c9f1f8df5003ef1ed8c0e38a247c850fb87b5e
villefrancisco <93373305+villefrancisco@users.noreply.github.com>
2021-11-05 13:23:34 -0600

    primer Intento

---
+++ src/lab4p2_franciscovilleda_12111170/Lab4P2_FranciscoVilleda_12111170.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4p2_franciscovilleda_12111170;
+
+/**
+ *
+ * @author JOSE VILLEDA
+ */
+public class Lab4P2_FranciscoVilleda_12111170 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
